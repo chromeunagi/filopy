6edef454657d819f6a2bb03b0151d7d8d8f55ad6
Sam Gamer <samgamer95@gmail.com>
2016-09-17 17:34:50 -0700

    pimped out commands

--- filopy.go
+++ filopy.go
@@ -8,14 +8,36 @@ import (
 	"strings"
 )
 
+// TODO: possibly add a help message for each cmd?
+type Command struct {
+	name string
+	argc int
+}
+
 var (
-	// TODO make a command struct. (name, arg count, etc)
-	commands = []string{"lock", "unlock", "exit"}
+	cmdLock   = Command{"lock", 1}
+	cmdUnlock = Command{"unlock", 1}
+	cmdExit   = Command{"exit", 0}
+
+	commands = []Command{cmdLock, cmdUnlock, cmdExit}
 )
 
 // Output a help message to stdout.
 func printHelp() {
-	fmt.Printf("Valid commands: %s\n", commands)
+	for _, c := range commands {
+		fmt.Printf("Command: %s, Args: %d\n", c.name, c.argc)
+	}
+}
+
+// Returns whether the given string represents the name of a
+// valid command.
+func isValidCmd(name string) bool {
+	for _, c := range commands {
+		if name == c.name {
+			return true
+		}
+	}
+	return false
 }
 
 // Run the CLI in a loop until the user exits.
@@ -36,13 +58,20 @@ func runCLI() {
 
 		tokens = strings.Fields(input)
 
-		// Filter out invalid commands.
+		// Filter out empty input.
 		if len(tokens) < 1 {
 			printHelp()
 			continue
 		}
 
+		// Filter out invalid commands.
+		// TODO: make this more specific. check for arglength, etc
 		cmd = tokens[0]
+		if !isValidCmd(cmd) {
+			printHelp()
+			continue
+		}
+
 		fmt.Printf("cmd: %s, args: %s\n", cmd, tokens[1:])
 
 	}
